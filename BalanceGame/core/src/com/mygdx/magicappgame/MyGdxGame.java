8b581725f242271059554510fd6e12bef0de0741
JiayinQu <jqu@macalester.edu>
2017-02-09 00:22:01 -0600

    Public/

---
+++ BalanceGame/core/src/com/mygdx/magicappgame/MyGdxGame.java
@@ -0,0 +1,43 @@
+package com.mygdx.magicappgame;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	private Box2DDebugRenderer debugRenderer;
+	private World world;
+	
+	@Override
+	public void create () {
+		Box2D.init();
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+
+	@Override
+	public void show(){
+		world = new World(new Vector2(0),)
+	}
+}
---
+++ BalanceGame/core/src/com/mygdx/magicappgame/States/State.java
@@ -0,0 +1,19 @@
+package com.mygdx.magicappgame.States;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector3;
+
+/**
+ * Created by Jiayin Qu on 2017/2/8.
+ */
+//public abstract class State {
+//    protected OrthographicCamera cam;
+//    protected Vector3 mouse;
+//    protected GamStateManager gam;
+//
+//    protected State(GamStateManager gam){
+//        this.gam = gam;
+//        cam = new OrthographicCamera();
+//        mouse = new Vector3();
+//    }
+//}
