247978fda252369c1189e412947a95ca2e821d67
JiayinQu <jqu@macalester.edu>
2017-02-14 18:35:24 -0600

    Start Project with BG added

---
+++ Scenes/Hud.java
@@ -0,0 +1,32 @@
+package com.mygdx.magicappgame.Scenes;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mygdx.magicappgame.MyGdxGame;
+
+/**
+ * Created by Jiayin Qu on 2017/2/12.
+ */
+public class Hud {
+    public Stage stage;
+    private Viewport viewport;
+
+    public Hud(SpriteBatch sb){
+
+        viewport = new FitViewport(MyGdxGame.V_WIDTH,MyGdxGame.V_HEIGHT,new OrthographicCamera());
+        stage = new Stage(viewport, sb);
+
+        Table table = new Table();
+        table.top();
+        table.setFillParent(true);
+
+        stage.addActor(table);
+    }
+}
