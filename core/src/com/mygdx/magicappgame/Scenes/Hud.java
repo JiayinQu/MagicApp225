32a41930d8559f7aece4ca315d619523b14cb601
ahayes <ahayes@macalester.edu>
2017-02-22 13:58:47 -0600

    Created Shapes Package (platform and square classes) Made some test input to create shapes on screen (see handle input) Fixed the orientation from landscape to portrait for emulator mode

--- core/src/com/mygdx/magicappgame/Scenes/Hud.java
+++ core/src/com/mygdx/magicappgame/Scenes/Hud.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -15,13 +17,18 @@ import com.mygdx.magicappgame.MyGdxGame;
  * Created by Jiayin Qu on 2017/2/12.
  */
 public class Hud {
+
     public Stage stage;
     private Viewport viewport;
+
 
     public Hud(SpriteBatch sb){
 
         viewport = new FitViewport(MyGdxGame.V_WIDTH,MyGdxGame.V_HEIGHT,new OrthographicCamera());
         stage = new Stage(viewport, sb);
+
+
+
 
         Table table = new Table();
         table.top();
--- core/src/com/mygdx/magicappgame/States/PlayScreen.java
+++ core/src/com/mygdx/magicappgame/States/PlayScreen.java
@@ -1,19 +1,30 @@
 package com.mygdx.magicappgame.States;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.magicappgame.MyGdxGame;
 import com.mygdx.magicappgame.Scenes.Hud;
+import com.mygdx.magicappgame.Shapes.Platform;
+import com.mygdx.magicappgame.Shapes.Square;
 
 /**
  * Created by Jiayin Qu on 2017/2/11.
@@ -27,9 +38,15 @@ public class PlayScreen implements Screen{
     private TmxMapLoader maploader;
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
+    private ShapeRenderer shapeRenderer;
 
     private World world;
     private Box2DDebugRenderer b2dr;
+    private Platform platform;
+    private Square box;
+    private Square box2;
+
+
 
     public PlayScreen(MyGdxGame game){
         this.game = game;
@@ -40,10 +57,16 @@ public class PlayScreen implements Screen{
         maploader = new TmxMapLoader();
         map = maploader.load("SkyBackG.tmx");
         renderer = new OrthogonalTiledMapRenderer(map);
+        shapeRenderer = new ShapeRenderer();
         gamecam.position.set(gamePort.getWorldWidth()/2,gamePort.getWorldHeight()/2,0);
 
-        world = new World(new Vector2(0,0),true);
+        world = new World(new Vector2(0,-9.8f),true);
         b2dr = new Box2DDebugRenderer();
+
+        platform = new Platform(world);
+
+
+
 
     }
 
@@ -52,15 +75,26 @@ public class PlayScreen implements Screen{
     }
 
     public void handleInput(float dt){
-        if(Gdx.input.isTouched())
-            gamecam.position.y +=100 *dt;
+//        if(Gdx.input.isTouched())
+//            gamecam.position.y +=100 *dt;
+        if(Gdx.input.isKeyJustPressed(Input.Keys.DOWN)){
+            box = new Square(world);
+            box.defineSquare(104, 300, 30, 30);
+        }
+        if(Gdx.input.isKeyJustPressed(Input.Keys.LEFT)){
+            box = new Square(world);
+            box.defineSquare(64, 300, 30, 30);
+        }
 
     }
+
     public void update(float dt){
+        world.step(1/60f, 6, 2);
         handleInput(dt);
 
         gamecam.update();
         renderer.setView(gamecam);
+        b2dr.render(world, gamecam.combined);
     }
 
     @Override
@@ -68,6 +102,7 @@ public class PlayScreen implements Screen{
         update(delta);
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 
         renderer.render();
 
