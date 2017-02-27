02d443c5365258220e272ce2850aea401adb6730
JiayinQu <jqu@macalester.edu>
2017-02-27 02:34:30 -0600

    Menu & Texture
    Added menu screen, still need to work on texture more. The texture
    cannot attach to the past objects because we call the same object again
    and again. Have added separate class for circles and square, the next
    step is to put different textures into different classes.

--- core/src/com/mygdx/magicappgame/States/PlayScreen.java
+++ core/src/com/mygdx/magicappgame/States/PlayScreen.java
@@ -3,29 +3,24 @@ package com.mygdx.magicappgame.States;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.magicappgame.MyGdxGame;
 import com.mygdx.magicappgame.Scenes.Hud;
+import com.mygdx.magicappgame.Shapes.Circles;
 import com.mygdx.magicappgame.Shapes.GenericShape;
 import com.mygdx.magicappgame.Shapes.Platform;
-import com.mygdx.magicappgame.Shapes.Square;
+import com.mygdx.magicappgame.Shapes.Squares;
 
 /**
  * Created by Jiayin Qu on 2017/2/11.
@@ -45,31 +40,33 @@ public class PlayScreen implements Screen{
     private Box2DDebugRenderer b2dr;
     private Platform platform;
     private GenericShape currentShape;
+    private Circles currentCircle;
 
     public Vector2 screenPos;
 
 
 
-    public PlayScreen(MyGdxGame game){
+    public PlayScreen(MyGdxGame game) {
         this.game = game;
         gamecam = new OrthographicCamera();
-        gamePort = new FitViewport(MyGdxGame.V_WIDTH,MyGdxGame.V_HEIGHT,gamecam);
+        gamePort = new FitViewport(MyGdxGame.V_WIDTH, MyGdxGame.V_HEIGHT, gamecam);
         hud = new Hud(game.batch);
 
         maploader = new TmxMapLoader();
         map = maploader.load("SkyBackG.tmx");
         renderer = new OrthogonalTiledMapRenderer(map);
         shapeRenderer = new ShapeRenderer();
-        gamecam.position.set(gamePort.getWorldWidth()/2,gamePort.getWorldHeight()/2,0);
+        gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
 
-        world = new World(new Vector2(0,-50f),true);
+        world = new World(new Vector2(0, -50f), true);
         b2dr = new Box2DDebugRenderer();
 
         platform = new Platform(world);
 
         screenPos = new Vector2(104, 300);
-
-
+        currentShape = new GenericShape(world, 0, 30, screenPos,this);
+        //currentShape = new Squares(world, 0, 30,screenPos,this);
+        //currentCircle = new Circles(world,1, 30, screenPos,this);
 
 
     }
@@ -89,10 +86,13 @@ public class PlayScreen implements Screen{
         }
 
         if(Gdx.input.isKeyJustPressed(Input.Keys.NUM_0)){
-            currentShape = new GenericShape(world, 0, 30, screenPos);
+            currentShape = new GenericShape(world, 0, 30, screenPos,this);
+            //currentShape = new Squares(world, 0, 30, screenPos,this);
         }
+
         if(Gdx.input.isKeyJustPressed(Input.Keys.NUM_1)){
-            currentShape = new GenericShape(world, 1, 30, screenPos);
+            currentShape = new GenericShape(world, 1, 30, screenPos,this);
+            //currentCircle = new Circles(world, 1, 30, screenPos,this);
         }
 
         // Moves the current falling shape to the left
@@ -109,10 +109,12 @@ public class PlayScreen implements Screen{
         handleInput(dt);
         world.step(1/60f, 6, 2);
 
+        gamecam.position.y = currentShape.bod.getPosition().y;
 
         gamecam.update();
         renderer.setView(gamecam);
         b2dr.render(world, gamecam.combined);
+
     }
 
     @Override
@@ -121,14 +123,15 @@ public class PlayScreen implements Screen{
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-
         renderer.render();
-
         b2dr.render(world,gamecam.combined);
+
+        game.batch.begin();
+        currentShape.render(game.batch);
+        game.batch.end();
 
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
-
     }
 
     @Override
