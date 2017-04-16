085ade7229efc9cc296c74f768b8278ec815db9f
ahayes <ahayes@macalester.edu>
2017-04-16 12:01:51 -0500

    Updated Level3 to make it more challenging. check it out fam

--- core/src/com/mygdx/magicappgame/levels/Level3.java
+++ core/src/com/mygdx/magicappgame/levels/Level3.java
@@ -22,15 +22,15 @@ public class Level3 extends Level{
      */
     private void initializeCoord(){
 
-        levelCoord.add(new Vector2(10, 50));
-        levelCoord.add(new Vector2(10, 50));
-        levelCoord.add(new Vector2(10, 50));
-        levelCoord.add(new Vector2(30, 30));
-        levelCoord.add(new Vector2(30, 30));
-        levelCoord.add(new Vector2(30, 30));
-        levelCoord.add(new Vector2(10, 10));
-        levelCoord.add(new Vector2(10, 10));
+        levelCoord.add(new Vector2(10, 20));
+        levelCoord.add(new Vector2(10, 30));
+        levelCoord.add(new Vector2(10, 40));
+        levelCoord.add(new Vector2(40, 40));
+        levelCoord.add(new Vector2(30, 10));
+        levelCoord.add(new Vector2(10, 30));
+        levelCoord.add(new Vector2(40, 10));
+        levelCoord.add(new Vector2(10, 40));
         levelCoord.add(new Vector2(50, 10));
-        levelCoord.add(new Vector2(50, 10));
+        levelCoord.add(new Vector2(10, 50));
     }
 }
