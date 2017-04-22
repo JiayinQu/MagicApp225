e16561188b226a1f54c6ad85cf0125a02488930e
Ansel Colby <dcolby1@macalester.edu>
2017-04-22 00:15:16 -0500

    Added the levelSelect buttons and fixed bugs
    ""

--- core/src/com/mygdx/magicappgame/States/LevelSelect.java
+++ core/src/com/mygdx/magicappgame/States/LevelSelect.java
@@ -5,9 +5,11 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -29,9 +31,9 @@ import java.util.ArrayList;
 public class LevelSelect implements Screen {
     private MyGdxGame game;
     private Stage stage;
-    private ArrayList <Label> levelList;
+    private ArrayList <Image> levelList;
     private int count;
-    private Label selectedLevel;
+    private Image selectedLevel;
 
     private OrthographicCamera camera;
     private Viewport viewport;
@@ -50,37 +52,39 @@ public class LevelSelect implements Screen {
         xPos = (viewport.getWorldWidth() / 2) - 25;
         yPos = viewport.getWorldHeight() / 7;
 
-        levelList = new ArrayList<Label>();
+        levelList = new ArrayList<Image>();
         setUpLevels();
         count = 1;
         selectedLevel = null;
     }
 
         private void setUpLevels() {
-            Label level1 = new Label("Level 1", MyGdxGame.gameSkin);
-            Label level2 = new Label("Level 2", MyGdxGame.gameSkin);
-            Label level3 = new Label("Level 3", MyGdxGame.gameSkin);
-            Label level4 = new Label("Level 4", MyGdxGame.gameSkin);
-            Label level5 = new Label("Level 5", MyGdxGame.gameSkin);
+            Image level1 = new Image(new Texture("tetriformButtonImages/level1.png"));
+            Image level2 = new Image(new Texture("tetriformButtonImages/level2.png"));
+            Image level3 = new Image(new Texture("tetriformButtonImages/level3.png"));
+            Image level4 = new Image(new Texture("tetriformButtonImages/level4.png"));
+            Image level5 = new Image(new Texture("tetriformButtonImages/level5.png"));
+
             levelList.add(level5);
             levelList.add(level4);
             levelList.add(level3);
             levelList.add(level2);
             levelList.add(level1);
 
-            for (final Label level:
+            for (final Image level:
                  levelList) {
-                level.setPosition(xPos, yPos*count + 75);
-                level.setWidth(50);
-                level.setHeight(40);
+                level.setPosition(xPos - level.getWidth()/6, yPos*count + 75);
+                level.setWidth(level.getWidth()/1.7f);
+                level.setHeight(level.getHeight()/1.7f);
                 stage.addActor(level);
 
                 level.addListener(new InputListener() {
                     public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                        Gdx.app.log("button", "pressed");
+                        level.setColor(Color.BLUE);
                         return true;
                     }
                     public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                        level.setColor(Color.WHITE);
                         selectedLevel = level;
                     }
                 });
